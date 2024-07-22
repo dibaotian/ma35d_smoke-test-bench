1e5151a39600b86b8b75b9d5c2ae54916809a9d4
minx <laba22@163.com>
2024-07-21 23:01:36 -0600

    update

--- run.sh
+++
@@ -1,4 +0,0 @@
-# DOCKER_ARGS=' ' \
-# DOCKER_IMAGE=packages.xilinx.com/docker-restricted/ma35/ubuntu_demo \
-# DOCKER_EXTRA="-v /home/xilinx/Documents/minx/ma35d_smoke-test-bench/videos:/demo/videos -v /home/xilinx/Documents/minx/ma35d_smoke-test-bench/test_scripts:/demo/test_scripts -v /home/xilinx/Documents/minx/ma35d_smoke-test-bench/tool_scripts:/demo/tool_scripts" \
-# /opt/amd/ma35/bin/launch
--- run_min.sh
+++
@@ -1,4 +0,0 @@
-# DOCKER_ARGS=' ' \
-# DOCKER_IMAGE=ma35_min \
-# DOCKER_EXTRA="-v /home/xilinx/Documents/minx/ma35d_smoke-test-bench/videos:/demo/videos -v /home/xilinx/Documents/minx/ma35d_smoke-test-bench/test_scripts:/demo/examples -v /home/xilinx/Documents/minx/ma35d_smoke-test-bench/tool_scripts:/demo/scripts" \
-# /opt/amd/ma35/bin/launch_min
